209a788718735491b3f8ab418fa08f496e734c75
Daniel Yang <danielysyang@gmail.com>
2023-05-26 20:23:40 +1200

    Improve shell script version

--- crypto-panel-widget.sh
+++ crypto-panel-widget.sh
@@ -4,14 +4,7 @@ url="https://api.binance.com/api/v3/ticker/price?symbols=\[\"BTCUSDT\",\"ETHUSDT
 data=$(curl -s "$url")
 
 if [[ $? -eq 0 ]]; then
-  coin_names=$(echo "$data" | jq -r '.[].symbol | sub("\\.?USDT$"; "")')
-  coin_prices=$(echo "$data" | jq -r '.[].price | sub("\\.?0*$"; "")')
-
-  # Combine coin names and prices with space
-  result=$(paste -d " " <(echo "$coin_names") <(echo "$coin_prices"))
-
-  # Separate each group by " | " and remove trailing " | "
-  printf "$result" | awk '{for (i=1; i<=NF; i+=2) printf "%s %s | ", $i, $(i+1)}' | sed 's/ | $//'
+  echo $data | jq -r '.[] | "\(.symbol | sub("\\.?USDT$"; "")) \(.price | sub("\\.?0*$"; ""))"' | awk '{ORS=" | "} {print}' | sed 's/ | $//'
 else
   echo "Failed to retrieve data from the URL."
 fi

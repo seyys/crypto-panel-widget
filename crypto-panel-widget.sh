8b01e8e42ba20b35b5910e2f79d5ba72c31bf576
Daniel Yang <danielysyang@gmail.com>
2024-10-24 21:08:43 +1100

    Add fallback urls

--- crypto-panel-widget.sh
+++ crypto-panel-widget.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-url="https://api.binance.com/api/v3/ticker/price?symbols=\[\"BTCUSDT\",\"ETHUSDT\",\"LINKUSDT\",\"ROSEUSDT\"\]"
-data=$(curl -s "$url")
+base_urls=(
+  "https://data-api.binance.vision"
+  "https://api.binance.com"
+  "https://api1.binance.com"
+  "https://api2.binance.com"
+  "https://api3.binance.com"
+  "https://api4.binance.com"
+)
 
-if [[ $? -eq 0 ]]; then
-  echo $data | jq -r '.[] | "\(.symbol | sub("\\.?USDT$"; "")) \(.price | sub("\\.?0*$"; ""))"' | awk '{ORS=" | "} {print}' | sed 's/ | $//'
-else
-  echo "Failed to retrieve data from the URL."
-fi
+endpoint="/api/v3/ticker/price?symbols=[\"BTCUSDT\",\"ETHUSDT\",\"LINKUSDT\",\"ROSEUSDT\"]"
 
+for url in "${base_urls[@]}"; do
+  data=$(curl -sg "$url$endpoint")
+  if [[ $? -eq 0 ]] && [[ -n "$data" ]]; then
+    echo $data | \
+      jq -r '.[] | "\(.symbol | sub("\\.?USDT$"; "")) \(.price | sub("\\.?0*$"; ""))"' | \
+      awk '{ORS=" | "} {print}' | \
+      sed 's/ | $//'
+    exit 0
+  fi
+done
+
+echo "Failed to retrieve data from the URL."
